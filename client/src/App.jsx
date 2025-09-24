bd837312a6de7ba629e21a90740a8f8f2fdde016
nzubechukwudavid <42248102+nzubechukwudavid@users.noreply.github.com>
2025-09-24 12:52:15 +0100

    added frameworks for channellistcontainer and channel container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer } from './components';
+
+import './App.css';
+
+const apiKey = 'spz529r7wxmx';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>NRC Chat App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer 
+        
+        />
+        <ChannelContainer
+
+        />
+      </Chat>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
