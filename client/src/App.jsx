f45f4bf9936da5a0db5b3b6c638779680531816c
nzubechukwudavid <42248102+nzubechukwudavid@users.noreply.github.com>
2025-09-29 16:24:30 +0100

    setup auth logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,24 @@ import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
 import './App.css';
 
+const cookies = new Cookies();
+
 const apiKey = 'spz529r7wxmx';
+const authToken = cookies.get('token');
 
 const client = StreamChat.getInstance(apiKey);
 
-const authToken = false;
+if (authToken) {
+  client.connectUser({
+    token: cookies.get('token'),
+    username: cookies.get('username'),
+    userID: cookies.get('userID'),
+    fullName: cookies.get('fullName'),
+    phoneNumber: cookies.get('phoneNumber'),
+    avatarUR: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword')
+  })
+}
 
 const App = () => {
   if(!authToken) return <Auth />
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -3,6 +3,8 @@ import Cookies from 'universal-cookie';
 import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg';
+const cookies = new Cookies();
+
 
 const initialState = {
   fullName: '',
@@ -23,10 +25,28 @@ const Auth = () => {
 
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
   
-    console.log(form);
+    const { fullName, username, password, phoneNumber, avatarURL } = form;
+    const URL = 'http://localhost:5000/auth';
+
+    const { data: { token, userID, hashedPassword } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+      username, password, fullName, phoneNumber, avatarURL
+    });
+
+    cookies.set('token', token);
+    cookies.set('username', username);
+    cookies.set('userID', userID);
+    cookies.set('fullName', fullName);
+
+    if (isSignup) {
+      cookies.set('phoneNumber', phoneNumber);
+      cookies.set('avatarURL', avatarURL);
+      cookies.set('hashedPassword', hashedPassword);
+    }
+
+    window.location.reload();
   }
 
   const switchMode = () => {
