bce138cc8ba2fc940667d78c496d24c4ebc5c8bf
nzubechukwudavid <42248102+nzubechukwudavid@users.noreply.github.com>
2025-09-29 12:08:16 +0100

    sign in and sign up logic

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+import signinImage from '../assets/signup.jpg';
+
+
+const Auth = () => {
+
+  const [isSignup, setIsSignup] = useState(true);
+
+  const handleChange = () => {}
+
+  return (
+    <div className="auth__form-container">
+      <div className="auth__form-container_fields">
+        <div className="auth__form-container_fields-content">
+          <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+          <form onSubmit={() => {}}>
+            {isSignup && (
+              <div className='auth__form-container_fields-content_input'>
+                <label htmlFor='fullName'>Full Name</label>
+                <input
+                  name='fullName'
+                  type='text'
+                  placeholder='Full Name'
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            <div className='auth__form-container_fields-content_input'>
+                <label htmlFor='username'>User Name</label>
+                <input
+                  name='username'
+                  type='text'
+                  placeholder='Username'
+                  onChange={handleChange}
+                  required
+                />
+              </div>  
+          </form>
+        </div>
+      </div>      
+    </div>
+  )
+}
+
+export default Auth
