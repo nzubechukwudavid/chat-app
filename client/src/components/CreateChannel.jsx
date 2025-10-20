a664cc2206e90e701087b00ef43d8a21ddfd78aa
nzubechukwudavid <42248102+nzubechukwudavid@users.noreply.github.com>
2025-10-20 14:24:37 +0100

    userList.jsx issues

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -22,27 +22,6 @@ const ChannelNameInput = ({ channelName = '', setChannelName }) => {
 
 const CreateChannel = ({ createType, setIsCreating }) => {
   const [channelName, setChannelName] = useState('');
-  const { client, setActiveChannel } = useChatContext();
-  const [selectedUsers, setSelectedUsers] = useState([client.userID || '']);
-
-  const createChannel = async (e) => {
-    e.preventDefault();
-
-    try {
-      const newChannel = await client.channel(createType, channelName, {
-        name: channelName, members: selectedUsers
-      });
-
-      await newChannel.watch();
-
-      setChannelName('');
-      setIsCreating(false);
-      setSelectedUsers([client.userID]);
-      setActiveChannel(newChannel);
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   return (
     <div className='create-channel__container'>
@@ -52,10 +31,6 @@ const CreateChannel = ({ createType, setIsCreating }) => {
       </div>
       {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
       <UserList />  
-      <UserList setSelectedUsers={setSelectedUsers} />
-      <div className='create-channel__button-wrapper' onClick={createChannel}>
-        <p>{createType === 'team' ? 'Create Channel' : 'Create Message Group'}</p>
-      </div>
     </div>
   )
 }
