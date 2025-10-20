2a035609c15c3edd4d3a875af8208e71cce2f6bb
nzubechukwudavid <42248102+nzubechukwudavid@users.noreply.github.com>
2025-10-20 14:20:17 +0100

    Implementing Add Channel

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -24,26 +24,6 @@ const CreateChannel = ({ createType, setIsCreating }) => {
   const [channelName, setChannelName] = useState('');
   const { client, setActiveChannel } = useChatContext();
   const [selectedUsers, setSelectedUsers] = useState([client.userID || '']);
-  const [channelName, setChannelName] = useState('');
-
-  const createChannel = async (e) => {
-    e.preventDefault();
-
-    try {
-        const newChannel = await client.channel(createType, channelName, {
-            name: channelName, members: selectedUsers
-        });
-
-        await newChannel.watch();
-
-        setChannelName('');
-        setIsCreating(false);
-        setSelectedUsers([client.userID]);
-        setActiveChannel(newChannel);
-    } catch (error) {
-        console.log(error);
-    }
-  }
 
   return (
     <div className='create-channel__container'>
@@ -53,10 +33,6 @@ const CreateChannel = ({ createType, setIsCreating }) => {
       </div>
       {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
       <UserList />  
-      <UserList setSelectedUsers={setSelectedUsers} />
-      <div className="create-channel__button-wrapper" onClick={createChannel}>
-            <p>{createType === 'team' ? 'Create Channel' : 'Create Message Group'}</p>
-      </div>
     </div>
   )
 }
